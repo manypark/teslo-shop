37bbf8b244b4749fa0e16508c69a56934db6f033
Manu <manypark@live.com>
2022-12-01 16:06:56 -0600

    conecction to db railway

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -10,9 +10,9 @@ export class CreateUserDto {
     @MinLength(6)
     @MaxLength(50)
     @Matches(
-    /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'The password must have a Uppercase, lowercase letter and a number'
-})
+        /(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'The password must have a Uppercase, lowercase letter and a number'
+    })
     password:string;
 
     @IsString()
